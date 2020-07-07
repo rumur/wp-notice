299132db67d1ad994efd8902f5960b8e2d16f2fb
Rumur <rumur.dev@gmail.com>
2020-07-07 17:04:05 +0200

    Introduced the NoticeTest

--- tests/Unit/NoticeTest.php
+++ tests/Unit/NoticeTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 class Notice extends TestCase
 {
-    public function canAddAndRenderNotices()
+    public function testCanAddAndRenderNotices()
     {
         \Rumur\WordPress\Notice\Notice::info($n1 = 'Info');
         \Rumur\WordPress\Notice\Notice::error($n2 = 'Error');
@@ -17,11 +17,43 @@ class Notice extends TestCase
         \Rumur\WordPress\Notice\Notice::render();
         $rendered = ob_get_clean();
 
-        var_dump($rendered);exit;
-
-        $this->assertStringContainsString('Test', $rendered);
+        $this->assertStringContainsString($n1, $rendered);
         $this->assertStringContainsString($n2, $rendered);
         $this->assertStringContainsString($n3, $rendered);
         $this->assertStringContainsString($n4, $rendered);
+        $this->assertStringNotContainsString('Wrong Message', $rendered);
+    }
+
+    public function testCanRenderWithConditions()
+    {
+        \Rumur\WordPress\Notice\Notice::info($n1 = 'Should be seen only once n1');
+        \Rumur\WordPress\Notice\Notice::error($n2 = 'Should not be seen at all n2')->showWhenPage('themes');
+        \Rumur\WordPress\Notice\Notice::warning($n3 = 'Should be seen once and later n3')->showLater(strtotime("+1sec"));
+        \Rumur\WordPress\Notice\Notice::success($n4 = 'Should be seen until n4')->showUntil('+1sec');
+
+        ob_start();
+        \Rumur\WordPress\Notice\Notice::render();
+        $rendered = ob_get_clean();
+
+        $this->assertStringContainsString($n1, $rendered);
+        $this->assertStringNotContainsString($n2, $rendered);
+        $this->assertStringNotContainsString($n3, $rendered);
+        $this->assertStringContainsString($n4, $rendered);
+
+        sleep(1);
+
+        ob_start();
+        \Rumur\WordPress\Notice\Notice::render();
+        $rendered = ob_get_clean();
+
+        $this->assertStringNotContainsString($n1, $rendered);
+        $this->assertStringContainsString($n3, $rendered);
+        $this->assertStringContainsString($n4, $rendered);
+
+        ob_start();
+        \Rumur\WordPress\Notice\Notice::render();
+        $rendered = ob_get_clean();
+
+        $this->assertStringNotContainsString($n3, $rendered);
     }
 }
